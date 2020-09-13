14e45c3622ca067ee265da3e1536c91c906b02cd
Brani Cohen <branircohen@gmail.com>
2020-09-13 16:23:03 -0400

    add optional attr on dog belonging to user and alias as owner

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,9 @@
 class Dog < ApplicationRecord
   has_many_attached :images
+
+  # Note: this assumes all owners are application users
+    # optional attribute added since we dont have owner information on all existing dog profiles
+  belongs_to :user, optional: true
+
+  alias_attribute :owner, :user
 end
