2091e5882333788d8c0cd4b0cd24ed496fd69204
Brani Cohen <branircohen@gmail.com>
2020-09-13 22:29:04 -0400

    use constant instead of ruby even method

--- app/helpers/dogs_helper.rb
+++ app/helpers/dogs_helper.rb
@@ -1,5 +1,7 @@
 module DogsHelper
+  IMG_COUNT_BEFORE_AD = 2
+
   def display_ad?(dog_counter)
-    dog_counter.even? && dog_counter > 1
+    dog_counter % IMG_COUNT_BEFORE_AD == 0 && dog_counter > 1
   end
 end
