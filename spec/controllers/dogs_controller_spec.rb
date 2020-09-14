ab102ca72207ec7b21d72d1c430047b7593ae44c
Brani Cohen <branircohen@gmail.com>
2020-09-13 21:23:22 -0400

    add test coverage for dog controller

--- spec/controllers/dogs_controller_spec.rb
+++ spec/controllers/dogs_controller_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe DogsController, type: :controller do
       get :index
       expect(assigns(:dogs).size).to eq(2)
     end
+
+    it 'displays up to 5 dogs per page' do
+      7.times { create(:dog) }
+      get :index
+      expect(assigns(:dogs).size).to eq(5)
+    end
   end
 end
+
