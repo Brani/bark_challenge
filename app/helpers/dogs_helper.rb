699c2856b13fcc5c3b787bd5b90912737f90c4c8
Brani Cohen <branircohen@gmail.com>
2020-09-13 22:34:46 -0400

    move owner check into helper

--- app/helpers/dogs_helper.rb
+++ app/helpers/dogs_helper.rb
@@ -4,4 +4,8 @@ module DogsHelper
   def display_ad?(dog_counter)
     dog_counter % IMG_COUNT_BEFORE_AD == 0 && dog_counter > 1
   end
+
+  def is_owner?
+    @dog.owner == current_user && current_user != nil
+  end
 end
