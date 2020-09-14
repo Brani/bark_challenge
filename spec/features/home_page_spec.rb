2ce3e58f7d0183f6a20ef69179f268cd9a66f666
Brani Cohen <branircohen@gmail.com>
2020-09-13 21:24:17 -0400

    add skeleton for test to display ads

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -12,4 +12,12 @@ RSpec.feature "Home page", :type => :feature do
     expect(page).to have_text("Sign in")
     expect(page).to have_text("Sign up")
   end
+
+  it 'displays an ad after every two dogs' do
+    5.times { create(:dog) }
+    visit "/"
+
+    # expect page to have third and sixth image with tag ad.jpg
+  end
 end
+
